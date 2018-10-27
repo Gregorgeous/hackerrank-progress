a96bba71d04b2b155da1179fa38f10a0b5c6db02
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2018-10-27 22:12:17 +0100

    Finished "New Year Chaos" challenge. Extra info #1: There's a potential room for enhancement: a) Takes up O(N) space for using extra dictionary of bribing numbers. b) Ultimately uses a variant of a bubble sort (but doesn't suffer from worst-case because of the limit of bribes set to 2 for each number). Extra info #2: Earned a "problem solver" bronze badge after solving this challenge.

--- Interview Prep Kit challenges/Arrays/New Year Chaos.py
+++ Interview Prep Kit challenges/Arrays/New Year Chaos.py
@@ -9,22 +9,23 @@ import sys
 # Complete the minimumBribes function below.
 def minimumBribes(q):
     bribes = 0
-    i = 0
     bribes_dict= {}
     bribes = 0
-    for index in range(1,len(q)):
-        if q[index] < q[index-1] :
-            i = index
-            while q[i] > q[i-1] and i > 0:
+    for index in range(0,len(q)-1):
+        if q[index] > q[index+1] :
+            i = index + 1
+            while  q[i-1] > q[i]and i > 0:
                 q[i], q[i-1] =q[i-1],q[i]
                 bribes += 1
-                if q[i-1] in bribes_dict:
-                    bribes_dict[q[i-1]] += 1
+                if q[i] in bribes_dict:
+                    bribes_dict[q[i]] += 1
                 else:
-                    bribes_dict[q[i-1]] = 1
-                if bribes_dict[q[i-1]] > 2:
+                    bribes_dict[q[i]] = 1
+                if bribes_dict[q[i]] > 2:
                     print ("Too chaotic")
                     return 
+                i -= 1
+    print(bribes)
     return bribes    
 
 if __name__ == '__main__':
